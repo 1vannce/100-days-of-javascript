789235d406ba50adb3f3fab3858fb04551e2e018
Ivan Rey Tagalicud <ivanrey.tagalicud@gmail.com>
2026-09-10 14:49:07 +0800

    Day 6: Feat: Add execution timing helper script

---
+++ .zed/run_helper.js
@@ -0,0 +1,13 @@
+import { spawnSync } from ("child_process");
+const t = performance.now();
+const res = spawnSync(process.execPath, [process.argv[2]], {
+  stdio: "inherit",
+});
+const s = ((performance.now() - t) / 1000).toFixed(3);
+console.log(
+  "\n[Done] exited with code=" +
+    (res.status !== null ? res.status : 0) +
+    " in " +
+    s +
+    " seconds",
+);
